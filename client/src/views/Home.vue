59c8c97a1724ccce08d055c4e486127f011ada13
Rachel Fish <itsrachelfish@gmail.com>
2021-05-15 16:16:51 -0600

    Created a helper function for outputting time

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,9 +1,11 @@
 <template>
   <div class="home">
-    <div v-if="state == 'stopped'">
-      Start Timer
+    <div class="text-xl mb-10">
+      {{ timerOutput }}
+    </div>
 
-      <span class="bg-red-600 p-4 rounded-xl">test</span>
+    <div v-if="state == 'stopped'">
+      <span class="bg-green-600 text-white font-bold p-4 rounded-xl">Start Timer</span>
     </div>
 
     <div v-else-if="state == 'running'">
@@ -31,5 +33,36 @@ export default {
       state: 'stopped',
     }
   },
+
+  computed: {
+    timerOutput() {
+      let timeRemaining = this.time;
+      let milliseconds = 0;
+      let seconds = 0;
+      let minutes = 0;
+      let hours = 0;
+
+      if(timeRemaining > 0) {
+        milliseconds = timeRemaining % 1000;
+        timeRemaining -= milliseconds;
+      }
+
+      if(timeRemaining > 0) {
+        seconds = (timeRemaining / 1000) % 60;
+        timeRemaining -= seconds * 1000;
+      }
+
+      if(timeRemaining > 0) {
+        minutes = (timeRemaining / (1000 * 60)) % 60;
+        timeRemaining -= minutes * 1000 * 60;
+      }
+
+      if(timeRemaining > 0) {
+        hours = timeRemaining / (1000 * 60 * 60);
+      }
+
+      return `${hours}h ${minutes}m ${seconds}s`;
+    },
+  }
 }
 </script>

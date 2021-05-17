179e6a2186d52f7b6f9ae8997a733b4ff3416ffd
Rachel Fish <itsrachelfish@gmail.com>
2021-05-17 00:15:25 -0600

    Created a meta-todo list page for planning the project

---
+++ client/src/views/Todo.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="todo">
+    <h1 class="text-2xl font-bold">Important Stuff Todo</h1>
+
+    <div class="inline-block text-left mb-8">
+      <ol class="list-decimal">
+        <li><s>Delete button for history rows</s></li>
+        <li>Show new history first</li>
+        <li>Filter history by date</li>
+        <li>Categorizing history into groups / tagging</li>
+        <li>Button for saving data to a file and importing</li>
+      </ol>
+    </div>
+
+    <h1 class="text-2xl font-bold">Medium term tasks</h1>
+
+    <div class="inline-block text-left mb-8">
+      <ol class="list-decimal">
+        <li>Starting a new timer when one is already paused</li>
+        <li>Ability to edit the time of a paused timer</li>
+        <li>Track the start and end time of each history entry</li>
+        <li>Add descriptions while a timer is runnning</li>
+        <li>Use modals instead of alert / confirm windows</li>
+      </ol>
+    </div>
+
+    <h1 class="text-2xl font-bold">Long term planning</h1>
+
+    <div class="inline-block text-left">
+      <ol class="list-decimal">
+        <li>Actually save history to a database</li>
+        <li>Sync timers across devices</li>
+      </ol>
+    </div>
+  </div>
+</template>
